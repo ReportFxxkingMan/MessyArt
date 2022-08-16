d6e4771fcc73fa7791d3fbc758a7190c4c7a3c93
RektPunk <rektpunk@gmail.com>
2022-08-17 00:28:38 +0900

    feat - add init and column name dict

---
+++ data/column_name.py
@@ -0,0 +1,11 @@
+COL_DICT = {
+    "결제일": "date",
+    "결제시간": "timestamp",
+    "결제내역": "content",
+    "합계": "amount",
+    "상품별 할인": "product_discount",
+    "결제 할인": "payment_discount",
+    "카드 결제": "card_payment",
+    "현금 결제": "cash_payment",
+    "간편 결제": "easy_payment",
+}
