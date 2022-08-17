9411049e96d384090b06759c5dfd8ebbf1ccfb87
RektPunk <110188257+RektPunk@users.noreply.github.com>
2022-08-17 15:41:52 +0900

    [Feature] - mock data (#2)
    * feat - change mock data into tsv
    
    * feat - remove mock data in csv
    
    * feat - add init and column name dict
    
    * feat - add example read
    
    * feat - add requirements.txt
    
    * feat - add eof and remove print df
    
    Co-authored-by: WonHyung Shin <wonhyung64@icloud.com>

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
-from oauth2client.service_account import ServiceAccountCredentials
 import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from data.column_name import COL_DICT
 
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
 
-json_key_path = "./cred/inner-geography-307404-91afac00e32e.json"
+def connectGoogleSheets():
+  scope = [
+      "https://spreadsheets.google.com/feeds",
+      "https://www.googleapis.com/auth/drive",
+  ]
 
-credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
-gc = gspread.authorize(credential)
+  json_key_path = "./cred/inner-geography-307404-91afac00e32e.json"
+
+  credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
+  gc = gspread.authorize(credential)
+  
+  return gc
+
+
+df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
+df = df.rename(columns=COL_DICT)
