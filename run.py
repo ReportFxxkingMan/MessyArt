3ed42f066c97556811741a9f5bc141c1cbb59cb2
WonHyung Shin <wonhyung64@icloud.com>
2022-08-18 21:55:38 +0900

    feat - reconstruction

--- run.py
+++ run.py
@@ -1,6 +1,29 @@
+#%%
 import pandas as pd
 from data.column_name import COL_DICT
+from module.database.google_sheet import connect_google_sheet
+from module.database.variable import (
+    SPREADSHEET_ID,
+    RANGE,
+    )
+
+def check_sheet(sheet, SPREADSHEET_ID, RANGE):
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE).execute()
+    values = result.get('values', [])
+    if not values:
+        print('No data found.')
+
+    for row in values:
+        print(row)
 
 
-df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
-df = df.rename(columns=COL_DICT)
+#%%
+if __name__ == "__main__":
+    df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
+    df = df.rename(columns=COL_DICT)
+
+    sheet = connect_google_sheet()
+    check_sheet(sheet, SPREADSHEET_ID, RANGE)
+
+# %%
