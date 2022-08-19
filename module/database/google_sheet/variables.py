8ea1d04a8a5343e0154323d5c45b64ebfbf85e6b
WonHyung Shin <wonhyung64@icloud.com>
2022-08-19 16:36:15 +0900

    feat - modify spreadsheet range

--- module/database/google_sheet/variables.py
+++ module/database/google_sheet/variables.py
@@ -1,3 +1,3 @@
 SPREADSHEET_ID = "1VyQ4jWCIONQ_aS198wowZeUeLZn860yhmgNVBmRab8c"
-SPREADSHEET_RANGE = "db!A1:J"
+SPREADSHEET_RANGE = "db!A1:I"
 SPREADSHEET_SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
