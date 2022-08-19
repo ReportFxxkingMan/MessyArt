4b1edecfb11da5bd71851b604a67f88bc7e1b606
WonHyung Shin <wonhyung64@icloud.com>
2022-08-19 16:28:58 +0900

    feat - add uploading data to spreadsheet function and reorganize check_sheet function

--- module/database/google_sheet/connect.py
+++ module/database/google_sheet/connect.py
@@ -1,6 +1,8 @@
 import os
 import logging
+import pandas as pd
 import googleapiclient
+from typing import Dict
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
@@ -34,3 +36,60 @@ def connect_google_sheet() -> googleapiclient.discovery.Resource:
     sheet = service.spreadsheets()
 
     return sheet
+
+
+def check_sheet(
+    sheet: googleapiclient.discovery.Resource,
+    SPREADSHEET_ID: str,
+    SPREADSHEET_RANGE: str,
+) -> pd.DataFrame:
+    """
+    check sheet's value from google spreadsheet api
+
+    Args:
+        sheet (googleapiclient.discovery.Resource): connected google sheet api
+        SPREADSHEET_ID (str): spreadsheet name to look up
+        SPREADSHEET_RANGE (str): spreadsheet range to look up
+
+    Returns:
+        pd.DataFrame: dataframe from google sheet
+    """
+    request = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE)
+    response = request.execute()
+    print("Successful Response")
+
+    values = response.get("values", [])
+    df = pd.DataFrame(values[1:], columns=values[0])
+
+    return df
+
+
+def data_to_sheet(
+    sheet: googleapiclient.discovery.Resource,
+    SPREADSHEET_ID: str,
+    SPREADSHEET_RANGE: str,
+    df: pd.DataFrame,
+) -> Dict:
+    """
+    upload data to google spreadsheet
+
+    Args:
+        sheet (googleapiclient.discovery.Resource): connected google sheet api
+        SPREADSHEET_ID (str): spreadsheet name to look up
+        SPREADSHEET_RANGE (str): spreadsheet range to look up
+        df (pd.DataFrame): data to upload
+
+    Returns:
+        Dict: upload response
+    """
+    request = sheet.values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=SPREADSHEET_RANGE,
+        valueInputOption="RAW",
+        insertDataOption="INSERT_ROWS",
+        body={"values": df.values.tolist()},
+    )
+    response = request.execute()
+    print("Successful Response")
+
+    return response["updates"]
