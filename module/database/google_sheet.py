c4abaaba199b68c8c8bd995016e184d0af9d9358
WonHyung Shin <wonhyung64@icloud.com>
2022-08-19 10:36:01 +0900

    feat - add typehint and Docstring

--- module/database/google_sheet.py
+++ module/database/google_sheet.py
@@ -1,11 +1,19 @@
 import os
 import logging
+import googleapiclient
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 from module.common.exceptions import GoogleCredentialException
 
-def connect_google_sheet():
+def connect_google_sheet() -> googleapiclient.discovery.Resource:
+    """
+    connect and authorize google api sheet
+
+    Returns:
+        googleapiclient.discovery.Resource: connected spreadsheet api
+    """
+    
     try:
         load_dotenv()
     except GoogleCredentialException as e:
