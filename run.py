1c0959eef8ef4f1fb5d5cbbfd11ae0c191b892ee
WonHyung Shin <wonhyung64@icloud.com>
2022-08-19 16:12:10 +0900

    hotfix - data_to_sheet arguments error

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 from data.column_name import COL_DICT
 from module.database.google_sheet import (
@@ -10,10 +11,13 @@ from module.database.variable import (
     SPREADSHEET_RANGE,
 )
 
+#%%
 if __name__ == "__main__":
     new_df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
     new_df = new_df.rename(columns=COL_DICT)
 
     sheet = connect_google_sheet()
     df = check_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE)
-    response = data_to_sheet(new_df, SPREADSHEET_ID, SPREADSHEET_RANGE)
+    response = data_to_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE, new_df)
+
+# %%
