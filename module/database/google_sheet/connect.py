c4d54c9353ca8cedb0cc27ed22c83139df06184f
WonHyung Shin <wonhyung64@icloud.com>
2022-08-20 21:01:35 +0900

    Feat/data upload2 (#8)
    * feat - add uploading data to spreadsheet function and reorganize check_sheet function
    
    * feat - reconstruction
    
    * feat - modify spreadsheet range
    
    * feat - modify spreadsheet range
    
    * feat - add data_to_sheet and reorganize
    
    * hotfix - module dir error
    
    * Update module/database/google_sheet/connect.py
    
    * Apply suggestions from code review
    
    Co-authored-by: RektPunk <110188257+RektPunk@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: RektPunk <110188257+RektPunk@users.noreply.github.com>
    
    * Update module/database/google_sheet/connect.py
    
    Co-authored-by: RektPunk <110188257+RektPunk@users.noreply.github.com>
    
    Co-authored-by: RektPunk <110188257+RektPunk@users.noreply.github.com>

--- module/database/google_sheet/connect.py
+++ module/database/google_sheet/connect.py
@@ -1,6 +1,8 @@
 import os
 import logging
+import pandas as pd
 import googleapiclient
+from typing import Dict
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
@@ -22,15 +24,68 @@ def connect_google_sheet() -> googleapiclient.discovery.Resource:
         logging.error(e)
 
     creds = Credentials.from_authorized_user_file(
-        filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"], 
+        filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
         scopes=SPREADSHEET_SCOPES,
     )
 
     service = build(
-        serviceName="sheets", 
-        version="v4", 
+        serviceName="sheets",
+        version="v4",
         credentials=creds,
     )
     sheet = service.spreadsheets()
 
     return sheet
+
+
+def check_sheet(
+    sheet: googleapiclient.discovery.Resource,
+    SPREADSHEET_ID: str,
+    SPREADSHEET_RANGE: str,
+) -> pd.DataFrame:
+    """
+    check sheet's value from google spreadsheet api
+    Args:
+        sheet (googleapiclient.discovery.Resource): connected google sheet api
+        SPREADSHEET_ID (str): spreadsheet name to look up
+        SPREADSHEET_RANGE (str): spreadsheet range to look up
+    Returns:
+        pd.DataFrame: dataframe from google sheet
+    """
+    request = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE)
+    response = request.execute()
+    print("Successful Response")
+
+    values = response.get("values", [])
+    df = pd.DataFrame(values[1:], columns=values[0])
+
+    return df
+
+
+def data_to_sheet(
+    sheet: googleapiclient.discovery.Resource,
+    SPREADSHEET_ID: str,
+    SPREADSHEET_RANGE: str,
+    df: pd.DataFrame,
+) -> Dict:
+    """
+    upload data to google spreadsheet
+    Args:
+        sheet (googleapiclient.discovery.Resource): connected google sheet api
+        SPREADSHEET_ID (str): spreadsheet name to look up
+        SPREADSHEET_RANGE (str): spreadsheet range to look up
+        df (pd.DataFrame): data to upload
+    Returns:
+        Dict: upload response
+    """
+    request = sheet.values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=SPREADSHEET_RANGE,
+        valueInputOption="RAW",
+        insertDataOption="INSERT_ROWS",
+        body={"values": df.values.tolist()},
+    )
+    response = request.execute()
+    print("Successful Response")
+
+    return response["updates"]
--- run.py
+++ run.py
@@ -1,43 +1,20 @@
 import pandas as pd
-import googleapiclient
-from module.database.google_sheet.connect import connect_google_sheet
+from data.column_name import COL_DICT
+from module.database.google_sheet.connect import (
+    connect_google_sheet,
+    check_sheet,
+    data_to_sheet,
+)
 from module.database.google_sheet.variables import (
     SPREADSHEET_ID,
     SPREADSHEET_RANGE,
 )
-from data.column_name import COL_DICT
-
-
-def check_sheet(
-    sheet: googleapiclient.discovery.Resource,
-    SPREADSHEET_ID: str,
-    SPREADSHEET_RANGE: str,
-) -> None:
-    """
-    check sheet's value from google spreadsheet api
-
-    Args:
-        sheet (googleapiclient.discovery.Resource): connected google sheet api
-        SPREADSHEET_ID (str): spreadsheet name to look up
-        SPREADSHEET_RANGE (str): spreadsheet range to look up
-    """
-
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE)
-        .execute()
-    )
-    values = result.get("values", [])
-    if not values:
-        print("No data found.")
-
-    for row in values:
-        print(row)
 
 
 if __name__ == "__main__":
-    df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
-    df = df.rename(columns=COL_DICT)
+    new_df = pd.read_table("data/mock_data.tsv", sep="\s{2,}", engine="python")
+    new_df = new_df.rename(columns=COL_DICT)
 
     sheet = connect_google_sheet()
-    check_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE)
+    df = check_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE)
+    response = data_to_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE, new_df)
