18a2f7c4c23778aaecb6fea12ae81f4be0a03c13
RektPunk <rektpunk@gmail.com>
2022-08-21 17:01:40 +0900

    feat - split code with feature and add type change script

--- module/database/google_sheet/connect.py
+++ module/database/google_sheet/connect.py
@@ -1,7 +1,5 @@
 import os
 import logging
-from typing import Dict
-import pandas as pd
 from dotenv import load_dotenv
 import googleapiclient
 from googleapiclient.discovery import build
@@ -28,64 +26,7 @@ def connect_google_sheet() -> googleapiclient.discovery.Resource:
         scopes=SPREADSHEET_SCOPES,
     )
 
-    service = build(
-        serviceName="sheets",
-        version="v4",
-        credentials=creds,
-    )
+    service = build(serviceName="sheets", version="v4", credentials=creds,)
     sheet = service.spreadsheets()
 
     return sheet
-
-
-def check_sheet(
-    sheet: googleapiclient.discovery.Resource,
-    SPREADSHEET_ID: str,
-    SPREADSHEET_RANGE: str,
-) -> pd.DataFrame:
-    """
-    check sheet's value from google spreadsheet api
-    Args:
-        sheet (googleapiclient.discovery.Resource): connected google sheet api
-        SPREADSHEET_ID (str): spreadsheet name to look up
-        SPREADSHEET_RANGE (str): spreadsheet range to look up
-    Returns:
-        pd.DataFrame: dataframe from google sheet
-    """
-    request = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE)
-    response = request.execute()
-    print("Successful Response")
-
-    values = response.get("values", [])
-    df = pd.DataFrame(values[1:], columns=values[0])
-
-    return df
-
-
-def data_to_sheet(
-    sheet: googleapiclient.discovery.Resource,
-    SPREADSHEET_ID: str,
-    SPREADSHEET_RANGE: str,
-    df: pd.DataFrame,
-) -> Dict:
-    """
-    upload data to google spreadsheet
-    Args:
-        sheet (googleapiclient.discovery.Resource): connected google sheet api
-        SPREADSHEET_ID (str): spreadsheet name to look up
-        SPREADSHEET_RANGE (str): spreadsheet range to look up
-        df (pd.DataFrame): data to upload
-    Returns:
-        Dict: upload response
-    """
-    request = sheet.values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=SPREADSHEET_RANGE,
-        valueInputOption="RAW",
-        insertDataOption="INSERT_ROWS",
-        body={"values": df.values.tolist()},
-    )
-    response = request.execute()
-    print("Successful Response")
-
-    return response["updates"]
