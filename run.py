6b77184f6a6c190b9cd6086731414512ef180e4d
RektPunk <rektpunk@gmail.com>
2022-08-21 17:01:48 +0900

    feat - change run py

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from data.column_name import COL_DICT
-from module.database.google_sheet.connect import (
-    connect_google_sheet,
-    check_sheet,
-    data_to_sheet,
-)
+from module.database.google_sheet.connect import connect_google_sheet
+from module.database.google_sheet.load import load_sheet
+from module.database.google_sheet.write import append_sheet
 from module.database.google_sheet.variables import (
     SPREADSHEET_ID,
     SPREADSHEET_RANGE,
@@ -16,5 +14,5 @@ if __name__ == "__main__":
     new_df = new_df.rename(columns=COL_DICT)
 
     sheet = connect_google_sheet()
-    df = check_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE)
-    response = data_to_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE, new_df)
+    df = load_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE)
+    response = append_sheet(sheet, SPREADSHEET_ID, SPREADSHEET_RANGE, new_df)
